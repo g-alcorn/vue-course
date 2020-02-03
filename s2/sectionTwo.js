27a8709deb4509785253f7b3fb38b5df84b78f41
Griffin Alcorn <griffin.alcorn@gmail.com>
2020-02-03 12:48:59 -0500

    more event handlers and file renaming

--- s2/sectionTwo.js
+++ s2/sectionTwo.js
@@ -5,7 +5,10 @@ new Vue({
     variableText: 'type to change me',
     url: 'https://google.ca',
     counter: 0,
-    charCount: 0
+    charCount: 0,
+    buttonCounter: 0,
+    x: 0,
+    y: 0
   },
   methods: {
     sayHello: function() {
@@ -23,6 +26,13 @@ new Vue({
     onUrlClick: function(event) {
       this.counter++;
       this.sayHello();
+    },
+    onButtonClick: function(event) {
+      this.buttonCounter++;
+    },
+    updateCoordinates: function(event) {
+      this.x = event.clientX;
+      this.y = event.clientY;
     }
   }
 });
